9effac28626afb493c5e8c497d8146ea7455f631
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-07-04 12:28:38 -0300

    first commit

---
+++ src/components/Clubs.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+export default function Clubs() {
+    const [clubs, setClubs] = useState([]);
+
+    useEffect(() => {
+        fetchClub();
+    }, []);
+
+    const fetchClub = async () => {
+        try {
+            const response = await fetch(`https://api.cartola.globo.com/clubes`);
+            const data = await response.json();
+            setClubs(Object.values(data)); // Converting object to array
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className="content">
+            {clubs.map((club) => (
+                <div className="clubes" key={club.id}>
+                    <img src={club.escudos['60x60']} alt={club.nome} />
+                    <div className="name">
+                        <h3>{club.nome}</h3>
+                        <p>{club.apelido}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+  return(
+    <>
+        <header>
+            <img src="senai.png" alt="" />
+            <h1>Times Brasileiros</h1>
+            <h2>João Benvenutti - 3C</h2>
+        </header>
+    </>
+  )
+}
